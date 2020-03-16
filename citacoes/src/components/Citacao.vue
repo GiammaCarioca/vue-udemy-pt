b771378d3f0b5d3f47a1a47dc31fb82c1142532c
Thiago Giammattey <giammattey@gmail.com>
2020-03-16 15:56:33 -0300

    passa conteúdo via props

--- citacoes/src/components/Citacao.vue
+++ citacoes/src/components/Citacao.vue
@@ -1,20 +1,18 @@
 <template>
-    <div class="citacao">
-        
-    </div>
+	<div class="citacao">{{ texto }}</div>
 </template>
 
 <script>
 export default {
-
+	props: ['texto']
 }
 </script>
 
 <style scoped>
-    .citacao {
-        border: 1px solid #DDD;
-        background-color: rgba(0, 0, 0, .1);
-        padding: 25px;
-        max-width: 450px;
-    }
+.citacao {
+	border: 1px solid #ddd;
+	background-color: rgba(0, 0, 0, 0.1);
+	padding: 25px;
+	max-width: 450px;
+}
 </style>
